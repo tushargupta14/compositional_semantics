f95059ba3e0a5114308aa2566a447dd8a3843afd
tushargupta14 <tushariitkgp14@gmail.com>
2016-12-16 05:52:13 -0500

    Adding matrix creation files

---
+++ read_context_word.py
@@ -0,0 +1,123 @@
+import os
+import sys
+import numpy as np
+from collections import defaultdict
+import math
+import matplotlib.pyplot as plt
+
+set_tags_to_keep = set(["CD","JJ","JJR","JJS","NN","NNS","NNP","NNPS","RB","RBR","RBS","UH","VB","VBD","VBG","VBN","VBP","VBZ"])
+'''
+Description: function to read context words
+Input: path <path to folder containing context words>
+Output: 
+i) set_context_words, dict_word_count
+'''
+
+'''
+Description: function to read context words
+Input: path <path to folder containing context words>
+Output: 
+i) set_context_words, dict_word_count
+'''
+
+'''
+Description: function to read context words
+Input: path <path to folder containing context words>
+Output: 
+i) set_context_words, dict_word_count
+'''
+
+
+'''
+Description: function to read context words
+Input: path <path to folder containing context words>
+Output: 
+i) set_context_words, dict_word_count
+'''
+
+'''
+Description: function to read context words
+Input: path <path to folder containing context words>
+Output: 
+i) set_context_words, dict_word_count
+'''
+
+def read_context_words(path_to_context_words):
+    set_context_word_count = set()
+    set_word_count = set()
+    
+    j = 0
+    
+    # reading the tags from file. if the tags are in tags_to_keep then appending the count of the word
+    with open(path_to_context_words + "pos_tag_dictionary.csv") as fp:
+        
+        for line in fp:
+            print j
+            j += 1
+            temp = defaultdict(int)
+            row = line.split('|')
+            temp['word'] = row[0]
+            tags = row[1].split(',')
+            for i in tags:
+                if i in set_tags_to_keep:
+                    temp['count'] += 1
+            set_word_count.add((temp['word'], temp['count']))
+            
+    #writing word count to file
+    fp = open(path_to_context_words + "word_count.csv", "wb+")
+    
+    for i in set_word_count:
+        fp.write(i[0] + "," + str(i[1]) + "\n")
+    fp.close()
+    
+    
+#     j = 0
+#     for i in set_word_count:
+#         print j
+#         j += 1
+#         temp = defaultdict(int)
+#         temp['word'] = k
+#         for i in v[0]:
+#             if i in set_tags_to_keep:
+#                 temp['count'] += 1
+#         list_word_count.append(temp)
+    
+    
+    # creating 
+    default_dict_freq_count = defaultdict(int)
+    for i in set_word_count:
+        default_dict_freq_count[i[1]] += 1
+    list_freq = list(default_dict_freq_count.keys())
+    len_list_freq = len(list_freq)
+    
+    
+    #keys
+    sorted_list_freq = sorted(list_freq)
+    
+    #values
+    sorted_list_count = [default_dict_freq_count[sorted_list_freq[i]] for i in xrange(len_list_freq)]
+    
+    count = 0
+    index = None
+    for i in xrange(len_list_freq):
+        count += sorted_list_count[len_list_freq - 1 - i]
+        if count > 40000:
+            index = len_list_freq - 1 - i
+            break
+    
+    threshold_freq = sorted_list_freq[index]
+    
+    for i in set_word_count:
+        if i[1] > threshold_freq:
+            set_context_word_count.add((i[0], i[1]))
+    
+    fp = open("context_word.csv", "wb+")
+    j = 0
+    for i in set_context_word_count:
+        j += 1
+        if j < 5:
+            print i[0], i[1]
+        fp.write(str(i[0]) + "," + str(i[1]) + "\n")
+    fp.close()
+        
+    return set_context_word_count, set_word_count
---
+++ read_wiktionary_suffix_source_word.py
@@ -0,0 +1,81 @@
+import os
+import sys
+import numpy as np
+from collections import defaultdict
+import math
+import matplotlib.pyplot as plt
+import json
+
+'''
+Description: function to read content words, suffixes and target words from files in folder given by path
+Input: path <path to folder containing suffix files>
+suffix files are named as "-<suffix>" eg. -age, -ly etc.
+the files are csv with each row as "<wiktionary link to the derived word>,<derived word>,<source word>,<suffix>"
+Output: 
+i) dict_content_words (a dictionary with the affix as key, and a list of words containing that affix, as value)
+ii) set_source_words (a set of all source words)
+iii) set_suffixes (a set of all suffixes)
+'''
+
+
+def read_wiktionary_suffix_source_word(path_to_suffix_list):
+    
+    
+    files = os.listdir(path_to_suffix_list)
+    
+    # initialize return values
+    dict_suffix_derived_words = defaultdict(set)
+    set_suffixes = set()
+    set_derived_words = set()
+    set_source_words = set()
+    
+    count = 0
+    corrupt = 0
+    for f in files:
+        
+        
+        # in file suffix is stored as "-<suffix" eg. "-age", "-ly". Removing the "-"
+        suffix = f[1:]
+        dict_suffix_derived_words[suffix] = []
+        set_suffixes.add(suffix)
+        
+        with open(path_to_suffix_list + f) as fp:
+            for line in fp:
+                count += 1
+                row = line.split(',')
+                
+                
+                # row is corrupt, ignore the row
+                if len(row) < 4:
+                    corrupt += 1
+                    continue
+                
+                
+                dict_suffix_derived_words[suffix].append(row[1])
+                set_source_words.add(row[2])
+                set_derived_words.append(row[2])
+        print count, corrupt
+    fp = open("suffixes.csv", "wb+")
+    
+    for i in set_suffixes:
+        fp.write(i + "\n")
+    
+    fp.close()
+    
+    fp = open("source_words.csv", "wb+")
+    
+    for i in set_source_words:
+        fp.write(i + "\n")
+    fp.close()
+    
+    fp = open("derived_words.csv", "wb+")
+    
+    for i in set_derived_words:
+        fp.write(i + "\n")
+    fp.close()
+    
+    
+    fp = open("suffix_derived_words.json", "wb+")
+    json.dump(dict_suffix_derived_words, fp)
+    fp.close()
+    return dict_suffix_derived_words, set_suffixes, set_source_words, set_derived_words
